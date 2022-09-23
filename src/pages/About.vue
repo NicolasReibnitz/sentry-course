5baab7dbc08db760f626815637250f1be6ef59f3
Nicolas Reibnitz <nicolas.reibnitz@simpleshow.com>
2022-09-22 22:00:30 -0400

    my code is perfect

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,6 +1,11 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-	<div>
-		<h1 class="text-4xl">About</h1>
-	</div>
+  <div>
+    <h1 class="text-4xl">About</h1>
+    <button @click="user.email.send()">Send Email</button>
+  </div>
 </template>
+
+<script setup>
+const user = {};
+</script>
--- vite.config.js
+++ vite.config.js
@@ -6,36 +6,36 @@ const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, ".env.local") });
 
 const viteSentryConfig = {
-	// url: "https://sentry.io",
-	authToken: process.env.SENTRY_AUTH_TOKEN,
-	org: "daslaboratory",
-	project: "vueschool-course",
-	release: `${process.env.npm_package_name}@${process.env.npm_package_version}`,
-	// deploy: {
-	// 	env: "production",
-	// },
-	setCommits: {
-		// auto: true,
-	},
-	sourceMaps: {
-		include: ["./dist/assets"],
-		ignore: ["node_modules"],
-		urlPrefix: "~/assets",
-	},
+  // url: "https://sentry.io",
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  org: "daslaboratory",
+  project: "vueschool-course",
+  release: `${process.env.npm_package_name}@${process.env.npm_package_version}`,
+  // deploy: {
+  // 	env: "production",
+  // },
+  setCommits: {
+    auto: true,
+  },
+  sourceMaps: {
+    include: ["./dist/assets"],
+    ignore: ["node_modules"],
+    urlPrefix: "~/assets",
+  },
 };
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
-	plugins: [vue(), viteSentry(viteSentryConfig)],
-	build: {
-		sourcemap: "hidden",
-	},
-	define: {
-		__SENTRY_RELEASE__: `"${process.env.npm_package_name}@${process.env.npm_package_version}"`,
-	},
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  plugins: [vue(), viteSentry(viteSentryConfig)],
+  build: {
+    sourcemap: "hidden",
+  },
+  define: {
+    __SENTRY_RELEASE__: `"${process.env.npm_package_name}@${process.env.npm_package_version}"`,
+  },
 });
